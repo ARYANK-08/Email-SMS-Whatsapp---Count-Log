82bf9cb64c60dbba92b2ac33747e24a844a3e5bc
Aryan Kyatham <120780784+ARYANK-08@users.noreply.github.com>
2023-12-29 23:46:57 +0530

    Update keys.py

--- Task10/keys.py
+++ Task10/keys.py
@@ -1,5 +1,5 @@
-account_sid = 'AC8ff1c391e4daaab4fd0a0d0faa4bab65'
-auth_token = '2df97aeb016072dc37db9675d18e90b0'
+account_sid = ''
+auth_token = ''
 
 twilio_number = '+14067204089'
 target_number = '+918657689680'
